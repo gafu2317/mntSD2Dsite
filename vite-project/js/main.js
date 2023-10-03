b022b0eab4d4000bd7cbe026f50e0b2ed7e6584b
gafu2317 <0002317rynkmk@gmail.com>
2023-10-04 01:27:10 +0900

    game1とgame2を作った

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,15 @@
+document.getElementById("button1").addEventListener("click", function () {
+  window.location.href = "game1.html"; // ゲーム1のページに遷移
+});
 
+document.getElementById("button2").addEventListener("click", function () {
+  window.location.href = "game2.html"; // ゲーム2のページに遷移
+});
 
+document.getElementById("button3").addEventListener("click", function () {
+  window.location.href = "game3.html"; // ゲーム3のページに遷移
+});
 
-
+document.getElementById("button4").addEventListener("click", function () {
+  window.location.href = "game4.html"; // ゲーム4のページに遷移
+});
